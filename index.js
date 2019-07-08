f9253dae57df498fa064fd256ac677ea1c513d13
Azzam Athar <33406171+Azzycodes@users.noreply.github.com>
2019-07-08 09:28:30 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const sumItems = function(arr) {
+  let sum = 0;
+  for (const item of array) {
+    if (Array.isArray(item) === true) {
+      sum += sumItems(item);
+    } else  {
+      sum += parseInt(item);
+    }
+  }
+  return sum;
+};
+
+module.exports = sumItems;
